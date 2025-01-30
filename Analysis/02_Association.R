2be525a759c25c8a072a4b7d974337d9bad34511
nmshintaku <nmshintaku@gmail.com>
2025-01-29 21:25:51 -0600

    update associations

--- Analysis/02_Association.R
+++ Analysis/02_Association.R
@@ -5,12 +5,14 @@
 library(SocGen)
 library(igraph)
 library(dplyr)
+library(tidyr)
 
 indiv_covars <- read.csv("Outputs/individual_covariates.csv")
 indiv_covars$entrydate <- as.Date(indiv_covars$entrydate)
 indiv_covars$departdate <- as.Date(indiv_covars$departdate)
 
 sightings <- read.csv("Outputs/sightings.csv")
+pairwise <- read.csv("Outputs/pairwise_covariates.csv")
 
 dates <- sort(unique(sightings$Observation.Date))
 
@@ -74,7 +76,9 @@ masked_network[is.na(masked_network)] <- 0
 network <- graph_from_adjacency_matrix(masked_network, mode = "undirected", weighted = TRUE)
 
 # calculate strength (summed edge weights) per ID
-strength(network)
+tot_str <- strength(network)
+tot_str_df <- data.frame(name = V(network)$name, strength = tot_str)
+#write.csv(tot_str_df, "Outputs/total_strength.csv", row.names = FALSE)
 
 #########
 ##Repro##
@@ -101,13 +105,28 @@ repro_network <- graph_from_adjacency_matrix(repro_mask_network, mode = "undirec
 
 str_repro <- strength(repro_network)
 str_repro_df <- data.frame(name = V(repro_network)$name, strength = str_repro)
-write.csv(str_repro_df, "Outputs/repro_strength.csv", row.names = FALSE)
+#write.csv(str_repro_df, "Outputs/repro_strength.csv", row.names = FALSE)
 
+str_repro_df <- str_repro_df %>%
+  separate(name, into = c("Dolphin.ID", "type"), sep = "\\.") %>%
+  spread(key = type, value = strength) %>%
+  replace_na(list(cyc = 0, lact = 0, preg = 0, juvenile = 0, unknown = 0)) %>%
+  rename(lact.strength = lact, cyc.strength = cyc, preg.strength = preg, repro.juvenile.strength = juvenile, repro.unknown.strength = unknown)
+
+#Not sure how to merge into affil_females
+affil_females <- merge(affil_females, str_repro_df, by)
+
+#CODE NOT WORKING 
 merge_repro_str <- merge(sightings, str_repro_df, by.x = "Repro.ID", by.y = "name", all.x = TRUE)
 
 #Setting vertex attributes to each reproductive category
 repro_network <- set_vertex_attr(repro_network, "ReproCat",
                                  value = sightings$ReproCat[match(V(repro_network)$name, sightings$Repro.ID)])
+
+#Setting edge attributes for close kin 
+#Need to have matching Repro.ID column in pairwise 
+repro_network <- set_edge_attr(repro_network, "CloseKin",
+                               value = pairwise$Closekin[match(V(repro_network)$name, pairwise$IDPair)])
 
 #OTHER WAY TO SET VERTEX ATTRIBUTE
 #matched_indices <- match(V(repro_network)$name, sightings$Repro.ID)
@@ -153,8 +172,19 @@ age_network <- graph_from_adjacency_matrix(age_mask_network, mode = "undirected"
 
 str_age <- strength(age_network)
 str_age_df <- data.frame(name = V(age_network)$name, strength = str_age)
-write.csv(str_age_df, "Outputs/age_strength.csv", row.names = FALSE)
+#write.csv(str_age_df, "Outputs/age_strength.csv", row.names = FALSE)
+
+str_age_df <- str_age_df %>%
+  separate(name, into = c("Dolphin.ID", "type"), sep = "\\.") %>%
+  spread(key = type, value = strength) %>%
+  replace_na(list(juvenile = 0, adult = 0, elder = 0)) %>%
+  rename(adult.strength = adult, juvenile.strength = juvenile, elder.strength = elder)
 
 #Setting vertex attributes to each age class
 age_network <- set_vertex_attr(age_network, "AgeClass",
                                  value = sightings$AgeClass[match(V(age_network)$name, sightings$Age.ID)])
+
+
+#Combining all the strength calculations into one dataframe
+all_strength <- merge(str_repro_df, str_age_df, by = "Dolphin.ID", all.x = TRUE)
+write.csv(all_strength, "Outputs/all_strength.csv", row.names = FALSE)
